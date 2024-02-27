632caaafbdf6eaa51b69249539958ce42860a24a
Jairo Rozan <jairo.rosa.z@hotmail.com>
2024-02-27 20:30:53 -0300

    update: header and footer into app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
     selector: "pkdex-root",
     standalone: true,
     templateUrl: "./app.component.html",
     styleUrl: "./app.component.scss",
-    imports: [RouterOutlet, NavbarComponent]
+    imports: [RouterOutlet, NavbarComponent, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = "proj-angular-pokedex";
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
-  selector: 'pkdex-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'pkdex-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss',
+    imports: [NavbarComponent]
 })
 export class HeaderComponent {
 
